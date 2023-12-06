8f514c48b67596a00b55efbe6ecc674f5adec84c
Xcoder <chindaebube03@gmail.comm>
2023-12-06 21:40:23 +0100

    set up configuration to start my app and database.

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // this is the starting point of your app
+import dotenv from "dotenv"
+import express from  "express"
+import {database} from "./config/dbConnect.js"
+
+dotenv.config();
+database();
+const app = express();
+app.use(express.json())
+
+
+
+const PORT = process.env.PORT || 8080;
+
+
+app.listen(PORT, console.log(` we are back  at port ${PORT} server is set.`))
--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,1 +1,2 @@
 // this is where is your database connection for your project is done.
+
