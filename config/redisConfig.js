49d058115526649443c2437a355af226b8b079db
Xcoder <chindaebube03@gmail.comm>
2023-12-06 22:37:24 +0100

    redis configuration

---
+++ config/redisConfig.js
@@ -0,0 +1,15 @@
+import redis from 'redis'
+// redisConfig.js
+
+
+const redisClient = redis.createClient({
+  host: 'localhost', // Redis server host
+  port: 6379,        // Redis server port
+  // Add other configuration options as needed
+});
+
+redisClient.on('error', (err) => {
+  console.error('Redis Error:', err);
+});
+
+module.exports = redisClient;
