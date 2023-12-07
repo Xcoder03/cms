1688249f66a7a3e515359194dd4c5e8f9a55b47d
Xcoder <chindaebube03@gmail.comm>
2023-12-07 10:01:26 +0100

    created and finished category route

---
+++ routes/route.category.js
@@ -0,0 +1,10 @@
+import express from "express";
+import * as categoryCtrl from  "../controllers/controller.category.js"
+import { isLogin } from "../middlewares/isLogin.js";
+import { isAdmin } from "../middlewares/isAdmin.js";
+
+const categoryRoutes =  express.Router();
+categoryRoutes.post("/create-category", isLogin, isAdmin, categoryCtrl.createCategory)
+categoryRoutes.get("/fetch-all-categories", isLogin, isAdmin, categoryCtrl.fetchCategories)
+categoryRoutes.put("/edit-category", isLogin, isAdmin, categoryCtrl.updateCategory)
+categoryRoutes.delete("/deleet-category", isLogin, isAdmin, categoryCtrl.deleteCategory)
