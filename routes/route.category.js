fbd603670df43aaf2684c384a43f450783763bda
Xcoder <chindaebube03@gmail.comm>
2023-12-07 11:27:53 +0100

    made an update to category route

--- routes/route.category.js
+++ routes/route.category.js
@@ -6,6 +6,6 @@ import { isAdmin } from "../middlewares/isAdmin.js";
 const categoryRoutes =  express.Router();
 categoryRoutes.post("/create-category", isLogin, isAdmin, categoryCtrl.createCategory)
 categoryRoutes.get("/fetch-all-categories", isLogin, isAdmin, categoryCtrl.fetchCategories)
-categoryRoutes.put("/edit-category", isLogin, isAdmin, categoryCtrl.updateCategory)
-categoryRoutes.delete("/deleet-category", isLogin, isAdmin, categoryCtrl.deleteCategory)
+categoryRoutes.put("/edit-category/:id", isLogin, isAdmin, categoryCtrl.updateCategory)
+categoryRoutes.delete("/delete-category/:id", isLogin, isAdmin, categoryCtrl.deleteCategory)
 export default categoryRoutes;
