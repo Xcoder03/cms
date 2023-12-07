9256240e05f8119b14afb190902965f5f6c93b13
Xcoder <chindaebube03@gmail.comm>
2023-12-07 09:53:27 +0100

    created and finished post routes

---
+++ routes/route.post.js
@@ -0,0 +1,11 @@
+import express from "express";
+import * as postCtrl from  "../controllers/controller.post.js"
+import { isLogin } from "../middlewares/isLogin.js";
+import { validateUser } from "../middlewares/userValidation.js";
+import { isAdmin } from "../middlewares/isAdmin.js";
+
+const postRoutes =  express.Router();
+postRoutes.post("/create-post", isLogin, postCtrl.createPost)
+postRoutes.get("/get-all-posts", isLogin, isAdmin, postCtrl.fetchAllPosts)
+postRoutes.put("/edit-post", isLogin, postCtrl.updatePostController)
+postRoutes.delete("/delete-post", isLogin, postCtrl.deletPost)
